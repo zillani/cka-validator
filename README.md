effcda821eaa6856acd327375f8159b272011f52
zillani <shaikzillani@gmail.com>
2022-11-21 01:54:19 +0530

    node express xterm init
    Signed-off-by: zillani <shaikzillani@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cka-validator
 Validate CKA answers/solution on k8s cluster. Uses go API to validate CKA scenarios
+
+### api
+
+```bash
+go mod vendor
+go mod download
+```
+
+### ui
+
+```bash
+npm install xterm
+npm install express
+```
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	router := gin.Default()
 
 	// Serve frontend static files
-	router.Use(static.Serve("/", static.LocalFile("./ui/views", true)))
+	router.Use(static.Serve("/", static.LocalFile("./ui/views/", true)))
 
 	// Setup route group for the API
 	api := router.Group("/api")
